b6ac4595b54c1ed264852ae1a779cd327b649d62
onizuka <findthefunction@gmail.com>
2024-06-11 02:40:07 +0800

    updated .gitignore

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Learning Journal Backend');
+});
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log(err));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
